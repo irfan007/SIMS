0385db9e98c126b3a28ba04eca7d6b76e95d9b9e
Kuldeep Rishi <kuldeepkrishi@gmail.com>
2014-02-26 12:12:40 +0530

    gitignore updated

--- sims/settings/base.py
+++ sims/settings/base.py
@@ -5,7 +5,7 @@ import os
 # holding the current file.
 here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
-PROJECT_ROOT = here("..")
+PROJECT_ROOT = here("../..")
 # root() gives us file paths from the root of the system to whatever
 # folder(s) we pass it starting at the parent directory of the current file.
 root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
@@ -109,6 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    root() + '/templates',
 )
 
 INSTALLED_APPS = (
--- sims/urls.py
+++ sims/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.views.generic import TemplateView
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    (r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chaining/', include('smart_selects.urls')),
 )
